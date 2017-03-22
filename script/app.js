178356b8600080a1e59129efc484490a45c590d0
ヨシュア <yss901025@gmail.com>
2017-03-22 10:02:36 +0800

    initScene

--- script/app.js
+++ script/app.js
@@ -7,6 +7,12 @@ let Colors = {
 	blue: 0x68c3c0,
 };
 
+let scene,
+    camera, fieldOfView, aspectRatio, nearPlane, farPlane,
+    WIDTH, HEIGHT,
+    renderer,
+    container;
+
 
 function init() {
     createScene();
@@ -21,6 +27,41 @@ function init() {
 }
 
 function createScene() {
+    WIDTH = window.innerWidth;
+    HEIGHT = window.innerHeight;
+
+    scene = new THREE.Scene();
+    scene.fog = new THREE.Fog(0xf7d9aa, 100, 950);
+
+    fieldOfView = 60;
+    aspectRatio = WIDTH / HEIGHT;
+    nearPlane = 1;
+    farPlane = 10000;
+    camera = new THREE.PerspectiveCamera(
+        fieldOfView,
+        aspectRatio,
+        nearPlane,
+        farPlane
+    );
+
+    camera.position.x = 0;
+    camera.position.y = 100;
+    camera.position.z = 200;
+
+    renderer = new THREE.WebGLRenderer({
+        alpha: true,
+        antialias: true,
+    });
+    renderer.setSize(WIDTH, HEIGHT);
+    renderer.shadowMap.enable = true;
+
+    container = document.getElementBtId('world');
+    container.appendChild(renderer.domElement);
+
+    window.addEventListener('resize', handleWindowResize, false);
+}
+
+function handleWindowResize() {
 
 }
 
